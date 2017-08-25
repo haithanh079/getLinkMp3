e2eafa56d0a532aa2ac48c8565158ac99f79e1dd
Thanh Hải <haithanh079@gmail.com>
2017-08-25 11:17:21 +0700

    api

--- bot.php
+++ bot.php
@@ -1,10 +1,26 @@
 <?php
 require_once __DIR__. DIRECTORY_SEPARATOR. 'vendor/autoload.php';
-use juno_okyo\Chatfuel;
-if (isset($_GET['msg']) && ! empty($_GET['msg']))
-{
-    (new Chatfuel())->sendText($_GET['msg']);
-}
+require_once __DIR__.'/getlink.php';
 
+use juno_okyo\Chatfuel;
+
+//if (isset($_GET['msg']) && ! empty($_GET['msg']))
+//{
+//    (new Chatfuel())->sendText($_GET['msg']);
+//}
+//nhận URL từ user
+$mp3Link=$_GET['msg'];
+//$mp3Link='http://mp3.zing.vn/bai-hat/1234-Chi-Dan/ZW7FE0FC.html';
+$getLink = new getLink();
+//Lấy link api
+$apiLink = $getLink->mp3Link($mp3Link);
+//Lấy link lossless
+$jsonLink = $getLink->generatorLink($apiLink);
+
+$mp3=$getLink->getLink320($jsonLink);
+
+$flac=$getLink->getLinkLossless($jsonLink);
+
+(new Chatfuel())->sendText($mp3);
 
 ?>
